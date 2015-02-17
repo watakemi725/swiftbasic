ad2fc75272e0c0bb46261c7cfd01812cd963b58c
Takemi Watanuki <takemi007@hotmail.com>
2015-02-17 17:10:49 +0900

    +

--- swift_basic/ViewController.swift
+++ swift_basic/ViewController.swift
@@ -15,6 +15,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         //labelを作成する
+        let mylabel: UILabel = UILabel(frame: CGRectMake(0, 0, 200, 50))
+        mylabel.backgroundColor = UIColor.orangeColor()
+        //枠をつける
+        mylabel.layer.masksToBounds = true
+        
+        //角をまるくする
+        mylabel.layer.cornerRadius = 20.0
+        
+        mylabel.text = "hello Swift"
+        
+        mylabel.textAlignment = NSTextAlignment.Center
+        
+        mylabel.textColor = UIColor.whiteColor()
+        
+        mylabel.shadowColor = UIColor.grayColor()
+        
+        mylabel.layer.position = CGPoint (x: self.view.bounds.width/2, y: 200)
+        
+        self.view.backgroundColor = UIColor.grayColor()
+        
+        
+        //重要::Viewに追加する
+        self.view.addSubview(mylabel)
+        
     }
 
     override func didReceiveMemoryWarning() {
